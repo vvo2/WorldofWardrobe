4b3f7c549b959d0485f66eee674f6c8eb7a257a5
vvo2 <vuongkv@gmail.com>
2018-02-27 11:49:22 -0700

    Initial commit

---
+++ app/src/main/java/edu/cnm/deepdive/worldofwardrobe/wardrobe/WardrobeStackItems.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive.worldofwardrobe.wardrobe;
+
+public class WardrobeStackItems {
+
+  Integer wardrobeIntImage;
+
+  public WardrobeStackItems(String name, Integer wardrobeIntImage) {
+    this.wardrobeIntImage = wardrobeIntImage;
+  }
+
+  public int getWardrobeIntImage() {
+    return wardrobeIntImage;
+  }
+}
